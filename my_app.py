128cb6ef4450bba685a14fa7807d031c657c97f3
komron01 <47138499+komron01@users.noreply.github.com>
2022-06-29 17:16:35 +0600

    v1.1
    1 экран работает, 2 тоже, но пустой

--- my_app.py
+++ my_app.py
@@ -1,8 +1,8 @@
 # напиши здесь код основного приложения и первого экрана
-from turtle import window_width
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
+from second_win import *
 
 class MainWin(QWidget):
     def __init__(self):
@@ -13,13 +13,30 @@ class MainWin(QWidget):
         self.show()
     
     def set_appear(self):
-        self.setWindowTitle(win1Title)
+        self.setWindowTitle(txt_title)
         self.resize(win_width,win_height)
         self.move(win_x,win_y)
+
     def initUI(self):
-        pass
+        self.hello_txt=QLabel(txt_hello)         #для текста добро пожаловать
+        self.instruction=QLabel(txt_instruction) # для текста инструкции
+        self.button=QPushButton(txt_next)        # для кнопки "начать"
+
+        self.Vlayout=QVBoxLayout()   # для вертикального расположение виджетов 
+        ''' добавление в вертикальное расположение виджетов '''
+        self.Vlayout.addWidget(self.hello_txt)
+        self.Vlayout.addWidget(self.instruction)
+        self.Vlayout.addWidget(self.button) 
+
+        self.setLayout(self.Vlayout) # установить расположение для окна 
+
+
     def connects(self):
-        pass    
+        self.button.clicked.connect(self.next_click)
+
+    def next_click(self):
+        self.hide()
+        self.tw=TestWin()    
 
 
 app=QApplication([])
