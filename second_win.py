41e024eb1ec5b67ded7546e8817b0c4cff767165
komron01 <47138499+komron01@users.noreply.github.com>
2022-06-29 17:17:07 +0600

    v1.1
    1 экран работает, второй пустой стоит

--- second_win.py
+++ second_win.py
@@ -1,2 +1,28 @@
 # напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from instr import *
+
+class TestWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width,win_height)
+        self.move(win_x,win_y)
+
+    def initUI(self):
+        pass
+
+
+    def connects(self):
+        pass
+
+    def next_click(self):
+        pass   
 
