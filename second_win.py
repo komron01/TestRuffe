393e59ae964395ffe0586679564320fd93f6a4a7
komron01 <47138499+komron01@users.noreply.github.com>
2022-07-01 17:29:19 +0600

    v1.2

--- second_win.py
+++ second_win.py
@@ -1,7 +1,10 @@
 # напиши здесь код для второго экрана приложения
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
+from PyQt5.QtGui import QFont
+
 from instr import *
+from final_win import *
 
 class TestWin(QWidget):
     def __init__(self):
@@ -17,12 +20,53 @@ class TestWin(QWidget):
         self.move(win_x,win_y)
 
     def initUI(self):
-        pass
+        self.name=QLabel(txt_name) 
+        self.name_editor=QLineEdit()
+        self.age=QLabel(txt_age)
+        self.age_editor=QLineEdit()     
+        self.instr1=QLabel(txt_test1)   
+        self.btn1=QPushButton(txt_starttest1)
+        self.test1=QLineEdit()
+        self.instr2=QLabel(txt_test2)   
+        self.btn2=QPushButton(txt_starttest2)
+        self.instr3=QLabel(txt_test3)   
+        self.btn3=QPushButton(txt_starttest3)
+        self.test2=QLineEdit()
+        self.test3=QLineEdit()
+        self.btn_res=QPushButton(txt_sendresults)
 
+        self.timer_txt=QLabel('00:00:00')
+        self.timer_txt.setFont(QFont("Times",36,QFont.Bold))
+
+        self.r_line=QVBoxLayout()
+        self.l_line=QVBoxLayout()
+        self.h_line=QHBoxLayout()
+
+        self.l_line.addWidget(self.name, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.name_editor, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.age, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.age_editor, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.instr1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.test1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.instr2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.instr3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.test2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.test3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_res, alignment = Qt.AlignRight)
+
+        self.r_line.addWidget(self.timer_txt, alignment = Qt.AlignCenter)
+
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+
+        self.setLayout(self.h_line)
 
     def connects(self):
-        pass
+        self.btn_res.clicked.connect(self.next_click)
 
     def next_click(self):
-        pass   
-
+        self.hide()
+        self.fw=FinalWin()   
