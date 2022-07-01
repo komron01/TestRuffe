432f2972880b5b11203aa7bbec907f0546ab9590
komron01 <47138499+komron01@users.noreply.github.com>
2022-07-01 17:29:02 +0600

    v1.2

--- my_app.py
+++ my_app.py
@@ -1,5 +1,5 @@
 # напиши здесь код основного приложения и первого экрана
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QLineEdit
 from instr import *
 from second_win import *
